56f74fd5569688a42831efff4a0d52327b1509d3
greentomatoma <greentomatoma@gmail.com>
2021-04-17 00:41:37 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday,        presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, :first_name, format: {with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: "Full-width characters"}
+    validates :last_name_kana,:first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "Full-width katakana characters"}
+    validates :birthday
+  end
+  PASSWORD_REGEX = /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/
+  validates :password, format: {with: PASSWORD_REGEX, message: "Include both letters and numbers"}
 
 end
