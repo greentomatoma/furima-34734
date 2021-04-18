1d8a92c00901e73515e48389cd778319cf4d7805
greentomatoma <greentomatoma@gmail.com>
2021-04-18 22:03:55 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+
   belongs_to :user
   has_one_attached :image
 
   with_options presence: true do
-    validates :name
-    validates :description, length: { maximum: 1000 }
-    validates :category_id
-    validates :status_id
-    validates :delivery_charge_id
-    validates :region_id
-    validates :delivery_days_id
+    validates :name,               length: { maximum: 40 }
+    validates :description,        length: { maximum: 1000 }
+    validates :category_id,        numericality: { other_than: 1 }
+    validates :status_id,          numericality: { other_than: 1 }
+    validates :delivery_charge_id, numericality: { other_than: 1 }
+    validates :region_id,          numericality: { other_than: 1 }
+    validates :delivery_days_id,   numericality: { other_than: 1 }
     validates :price, format: { with: /\A[0-9]+\z/ }, 
-                numericality: { :greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+                numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
 end
--- db/migrate/20210418021155_create_items.rb
+++ db/migrate/20210418021155_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :name,               null: false
-      t.text :description,          null: false, limit: 1000
-      t.string :category_id,        null: false
-      t.string :status_id,          null: false
-      t.string :delivery_charge_id, null: false
-      t.string :region_id,          null: false
-      t.string :delivery_days_id,   null: false
-      t.integer :price,             null: false
-      t.references :user,           null: false, foreign_key: true
+      t.string     :name,               null: false, limit: 40
+      t.text       :description,        null: false, limit: 1000
+      t.string     :category_id,        null: false
+      t.string     :status_id,          null: false
+      t.string     :delivery_charge_id, null: false
+      t.string     :region_id,          null: false
+      t.string     :delivery_days_id,   null: false
+      t.integer    :price,              null: false
+      t.references :user,               null: false, foreign_key: true
       t.timestamps
     end
   end
