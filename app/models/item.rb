d1c487f04ce71dff96442fad3f3663d644071638
greentomatoma <greentomatoma@gmail.com>
2021-04-18 15:21:23 +0900

    商品出品ページへの遷移

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,6 +11,6 @@ class Item < ApplicationRecord
     validates :region_id
     validates :delivery_days_id
     validates :price, format: { with: /\A[0-9]+\z/ }, 
-                numericality: { :greater_than_or_equal_to: 300, less_than_or_equal_to: 9*7}
+                numericality: { :greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   end
 end
