a86702819e33b20afdb69e40ff13301d3371d942
greentomatoma <greentomatoma@gmail.com>
2021-04-22 15:20:00 +0900

    controller, ActiveHash 修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,11 +12,11 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name,               length: { maximum: 40 }
     validates :description,        length: { maximum: 1000 }
-    validates :category_id
-    validates :status_id
-    validates :delivery_charge_id
-    validates :region_id
-    validates :delivery_days_id
+    validates :category_id,        numericality: { other_than: 0 }
+    validates :status_id,          numericality: { other_than: 0 }
+    validates :delivery_charge_id, numericality: { other_than: 0 }
+    validates :region_id,          numericality: { other_than: 0 }
+    validates :delivery_days_id,   numericality: { other_than: 0 }
     validates :price, format: { with: /\A[0-9]+\z/ }, 
                 numericality: { only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9_999_999 }
     validates :image
