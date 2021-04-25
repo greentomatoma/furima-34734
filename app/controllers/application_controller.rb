3216d55bd4bff46aa7559a3a1c937f66af7726bf
greentomatoma <greentomatoma@gmail.com>
2021-04-25 23:02:13 +0900

    テーブル修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['FURIMA_USER'] && password == ENV['FURIMA_PASSWORD']
+      username == ENV['FURIMA_USER'] && password == ENV['FURIMA_PASSWORD']
     end
   end
 
--- db/migrate/20210425020722_create_shipping_addresses.rb
+++ db/migrate/20210425020722_create_shipping_addresses.rb
@@ -6,7 +6,7 @@ class CreateShippingAddresses < ActiveRecord::Migration[6.0]
       t.string     :city,         null: false
       t.string     :block,        null: false
       t.string     :building,     null: false
-      t.integer    :phone_number, null: false
+      t.string     :phone_number, null: false
       t.references :purchase,     null: false, foreign_key: true
       t.timestamps
     end
