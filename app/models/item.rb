1bba076521766abeea80a2da66a7bd7b51c37b78
greentomatoma <greentomatoma@gmail.com>
2021-05-04 13:10:24 +0900

    エラー文 日本語対応

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,11 +13,11 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name,               length: { maximum: 40 }
     validates :description,        length: { maximum: 1000 }
-    validates :category_id,        numericality: { other_than: 0 }
-    validates :status_id,          numericality: { other_than: 0 }
-    validates :delivery_charge_id, numericality: { other_than: 0 }
-    validates :region_id,          numericality: { other_than: 0 }
-    validates :delivery_days_id,   numericality: { other_than: 0 }
+    validates :category_id,        numericality: { other_than: 0, message: 'を選択してください' }
+    validates :status_id,          numericality: { other_than: 0, message: 'を選択してください' }
+    validates :delivery_charge_id, numericality: { other_than: 0, message: 'を選択してください' }
+    validates :region_id,          numericality: { other_than: 0, message: 'を選択してください' }
+    validates :delivery_days_id,   numericality: { other_than: 0, message: 'を選択してください' }
     validates :price, format: { with: /\A[0-9]+\z/ },
                       numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :image
